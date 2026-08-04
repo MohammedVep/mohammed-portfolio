9a02dbb06ac6051399e7fddd2433d6fccaccc8b6
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2026-08-04 09:45:49 -0400

    Fixed experience and added hackathon

--- content/experience/index.ts
+++ content/experience/index.ts
@@ -9,5 +9,15 @@ export const experienceData: Experience[] = [
       "At Amazon Fulfillment, I worked very well in high volume fulfillment operations in a fast-paced environment where accuracy, safety, and consistent execution of warehouse work can impact customer experience. I process, and prepare customer orders quickly while following safety, and quality requirements.",
       "Over nearly ten years, I worked in changing workloads, priorities, and met different business needs. I was able to adapt quickly as order volumes skyrocket, assignments change and if teams need support in different areas ensuring work is done during high demanding periods such as prime and peak."
     ]
+  },
+  {
+    title: "InsureTech Hackathon",
+    company: "Pie & AI",
+    date: "August 2026",
+    description: [
+      "Designing proactive solution to give drivers a reward for following rules of the road, so that insurance premiums go down faster based on driving behavior, which means more savings for good drivers instantly, resulting in safer driving habits.",
+      "Building AI powered solution to help identify car insurance rates, so that premiums are decided based on accidents or tickets, which means higher premiums for traffic ticket, resulting in lower traffic violation rate.",
+      "Designing proactive rewards system to lower premiums based on changes made to client’s home, so insurance premiums go down faster based on improvements such as home alarm system, which means more savings for low-risk homeowners, resulting in lower burglary rates"
+    ]
   }
 ];
